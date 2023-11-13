79091149f6b4cf17a37d4762d0917d8ba571fa3a
Nagmani kumar <nag2mani@gmail.com>
2023-11-13 15:10:55 +0530

    models added

--- core/home/models.py
+++ core/home/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    # id = models.AutoField()
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    email = models.EmailField(null=True)
+    image = models.ImageField()
+    file = models.FileField()
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.IntegerField()
