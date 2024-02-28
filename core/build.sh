5575ad1ad15dc1160a86b079022e4723638af79a
nag2mani <nag2mani@gmail.com>
2024-02-28 11:27:30 +0530

    updated build.sh

--- core/build.sh
+++ core/build.sh
@@ -4,6 +4,8 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
-python manage.py migrate
+python3 manage.py collectstatic --no-input
+python3 manage.py migrate
+
+python3 manage.py runserver
 
